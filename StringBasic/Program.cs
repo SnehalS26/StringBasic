28f094ed4441072525d4b3e50363b7005b3ddb44
SnehalS26 <Snehalshirsath26@gmail.com>
2023-06-28 12:21:14 +0530

    Add project files.

---
+++ StringBasic/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBasic
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "Hello";
+            Console.WriteLine(str1);
+            Console.WriteLine(str1.GetHashCode());
+
+            str1 = "Hello to all";
+            string str2 = " Good Night";
+            Console.WriteLine(str1);
+            Console.WriteLine(str1.GetHashCode());
+
+            Console.WriteLine(str1.Length);
+            string str3 = string.Concat(str1, str2);
+            Console.WriteLine(str3.IndexOf('o'));
+            Console.WriteLine(str3.LastIndexOf('o'));
+            Console.WriteLine(str3);
+
+            string str4 = str3.Substring(2, 6);
+            Console.WriteLine(str4);
+
+            Console.WriteLine(String.IsNullOrEmpty(str4));
+
+            // string str5 = string.Empty;
+            string str5 = null;
+            Console.WriteLine(string.IsNullOrEmpty(str5));
+
+            Console.WriteLine("Enter name");
+            string s = Console.ReadLine();
+            Console.WriteLine(string.IsNullOrEmpty(s));
+            //char[] ch= str1.ToCharArray();
+            // for (int i = 0; i < ch.Length; i++)
+            // {
+            //     Console.WriteLine(ch[i]);
+            // }
+
+
+
+        }
+    }
+}
---
+++ StringBasic/StringBuilder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBasic
+{
+    public class StringBuilder
+    {
+       
+        static void Main(string[] args)
+        {
+             StringBuilder sb = new StringBuilder("Hello to All");
+            // or
+
+            StringBuilder sb2 = new StringBuilder(20);// capacity --> max no of characters that we want to store
+
+            // or
+
+
+
+            Console.WriteLine(sb);
+            Console.WriteLine(sb.GetHashCode());
+
+            sb.Append("Good Morning");
+            Console.WriteLine(sb);
+            Console.WriteLine(sb.GetHashCode());
+
+            sb.Insert(5, "TEST DATA");
+            sb.Remove(5, 4);
+
+            sb.Replace("DATA", "new data");
+            Console.WriteLine(sb);
+
+
+            StringBuilder sb3 = new StringBuilder("You amount is ");
+
+            //sb3.AppendFormat("{0:C}", 25000); // C --> currency
+
+            Console.WriteLine(sb3);
+
+        }
+    }
+}
